0a656524bc9c96ee1ec2d093f5578f4a594ed159
JasonLin <f1601413521@outlook.com>
2018-10-17 14:53:44 +0800

    新增Domain層領域物件基底類別與專案臨調服務公開介面。

---
+++ ThinkPower.CCLPA.Domain/Entity/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThinkPower.CCLPA.Domain.Entity
+{
+    /// <summary>
+    /// 領域物件基底類別
+    /// </summary>
+    public class BaseEntity
+    {
+    }
+}
---
+++ ThinkPower.CCLPA.Domain/Service/Interface/IProjectTemporaryAdjustment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThinkPower.CCLPA.Domain.Service.Interface
+{
+    /// <summary>
+    /// 專案臨調服務公開介面
+    /// </summary>
+    interface IProjectTemporaryAdjustment
+    {
+        /// <summary>
+        /// 預審名單匯入
+        /// </summary>
+        /// <param name="cmpnId">行銷活動代號</param>
+        /// <returns></returns>
+        object ImportPreTrialList(string cmpnId);
+
+        /// <summary>
+        /// 預審名單處理
+        /// </summary>
+        void PreTrialListProcessing();
+
+        /// <summary>
+        /// 臨調處理
+        /// </summary>
+        void Processing();
+    }
+}
