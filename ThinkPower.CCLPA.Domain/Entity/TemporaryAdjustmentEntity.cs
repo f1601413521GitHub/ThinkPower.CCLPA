8ec9855c6e29cbbc55206d60daf6cecce9774a6e
JasonLin <f1601413521@outlook.com>
2018-10-17 19:18:04 +0800

    新增專案臨調服務與「專案臨調預審名單匯入」相關Entity類別。

---
+++ ThinkPower.CCLPA.Domain/Entity/TemporaryAdjustmentEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace ThinkPower.CCLPA.Domain.Entity
+{
+    /// <summary>
+    /// 專案臨調類別
+    /// </summary>
+    public class TemporaryAdjustmentEntity
+    {
+        /// <summary>
+        /// 檢核預審名單結果
+        /// </summary>
+        public ValidatePreTrialEntity ValidatePreTrialResult { get; set; }
+    }
+}
--- ThinkPower.CCLPA.Domain/Service/Interface/IProjectTemporaryAdjustment.cs
+++ ThinkPower.CCLPA.Domain/Service/Interface/IProjectTemporaryAdjustment.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ThinkPower.CCLPA.Domain.Entity;
 
 namespace ThinkPower.CCLPA.Domain.Service.Interface
 {
@@ -12,11 +13,18 @@ namespace ThinkPower.CCLPA.Domain.Service.Interface
     interface IProjectTemporaryAdjustment
     {
         /// <summary>
+        /// 檢核預審名單
+        /// </summary>
+        /// <param name="cmpnId">行銷活動代號</param>
+        /// <returns></returns>
+        TemporaryAdjustmentEntity ValidatePreTrialList(string cmpnId);
+
+        /// <summary>
         /// 預審名單匯入
         /// </summary>
         /// <param name="cmpnId">行銷活動代號</param>
         /// <returns></returns>
-        object ImportPreTrialList(string cmpnId);
+        TemporaryAdjustmentEntity ImportPreTrialList(string cmpnId);
 
         /// <summary>
         /// 預審名單處理
