9819715ad9f9062760bb2f983cec80889c557a4d
JasonLin <f1601413521@outlook.com>
2018-10-18 10:23:29 +0800

    新增專案臨調控制器與預設首頁、移除HomeController與相關頁面、新增JavaScript主檔main.js。

---
+++ ThinkPower.CCLPA.Web/Controllers/TemporaryAdjustmentController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ThinkPower.CCLPA.Web.Controllers
+{
+    /// <summary>
+    /// 專案臨調
+    /// </summary>
+    public class TemporaryAdjustmentController : Controller
+    {
+        /// <summary>
+        /// 預設首頁
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
