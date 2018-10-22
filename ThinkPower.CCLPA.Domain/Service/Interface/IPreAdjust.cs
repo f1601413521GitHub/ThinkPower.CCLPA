3a7049e7ee482890749eee15722e2fa56682a45f
JasonLin <f1601413521@outlook.com>
2018-10-22 23:52:01 +0800

    新增預審服務與公開介面。

---
+++ ThinkPower.CCLPA.Domain/Service/Interface/IPreAdjust.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThinkPower.CCLPA.Domain.Service.Interface
+{
+    /// <summary>
+    /// 預審服務公開介面
+    /// </summary>
+    interface IPreAdjust
+    {
+    }
+}
---
+++ ThinkPower.CCLPA.Domain/Service/PreAdjustService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThinkPower.CCLPA.Domain.Service
+{
+    /// <summary>
+    /// 預審服務
+    /// </summary>
+    public class PreAdjustService
+    {
+    }
+}
